9d860073afca07dcc2bd91f5a7d7aca046e1cfe1
José Vitor <josev4854@gmail.com>
2023-01-10 11:51:15 -0300

    Correção

--- src/main/java/br/jvprojetos17/payrollapi/domain/Payroll.java
+++ src/main/java/br/jvprojetos17/payrollapi/domain/Payroll.java
@@ -11,7 +11,6 @@ public class Payroll {
 
     private String userName;
     private String description;
-    private Double hourlyPrice;
     private Double workedHours;
     private Double totalPayment;
 
